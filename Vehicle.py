a71d4bd3cd0ccf85ce934441c891d9d9b049c1f0
prestonchee <prestonchee1@gmail.com>
2022-11-29 18:42:16 -0700

    created paint and print functions in class

--- Vehicle.py
+++ Vehicle.py
@@ -1,22 +1,33 @@
 class Vehicle:
-    pass
 
-bug_object = Vehicle() #object of vehicle class -- instance of vehicle class
-turtle_object = Vehicle()
-rover_object = Vehicle()
+    #class level attribute
+    type = "ground"
+    propulsion = "battery"
 
-bug_object.color = "yellow"
-bug_object.num_wheels = 4
-bug_object.speed = 1
 
-turtle_object.color = "green"
-turtle_object.num_wheels = 2
-turtle_object.speed = 5
 
-rover_object.color = "purple"
-rover_object.num_wheels = 4
-rover_object.speed = 25
+    def __init__(self, name, color, num_wheels, speed):
+        self.name = name
+        self.color = color
+        self.num_wheels = num_wheels
+        self.speed = speed  
+        
+        
+    def print_details(self):
+        print(self.name,'is',self.color, 'and is able to drive', self.speed, 'mph.')
 
-print('This vehicle is',bug_object.color, 'and is able to drive', bug_object.speed, 'mph.')
-print('This vehicle is',turtle_object.color, 'and is able to drive', turtle_object.speed, 'mph.')
-print('This vehicle is',rover_object.color, 'and is able to drive', rover_object.speed, 'mph.')
+
+    def paint_vehicle(self,new_color):
+        self.color = new_color
+
+bug_object = Vehicle("beetle","yellow",4,1) #object of vehicle class -- instance of vehicle class
+turtle_object = Vehicle("turtlebot", "green",2,5)
+rover_object = Vehicle("rover","purple",4,25)
+
+bug_object.print_details()
+# turtle_object.print_details()
+# rover_object.print_details()
+
+bug_object.paint_vehicle("blue")
+
+bug_object.print_details()
