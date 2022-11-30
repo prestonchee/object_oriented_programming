be3f9392d5080e1b191fcb63af5e3ea996ade699
prestonchee <prestonchee1@gmail.com>
2022-11-29 18:50:49 -0700

    imported vehcile from vehicle folder

--- Vehicle.py
+++ Vehicle.py
@@ -20,14 +20,3 @@ class Vehicle:
     def paint_vehicle(self,new_color):
         self.color = new_color
 
-bug_object = Vehicle("beetle","yellow",4,1) #object of vehicle class -- instance of vehicle class
-turtle_object = Vehicle("turtlebot", "green",2,5)
-rover_object = Vehicle("rover","purple",4,25)
-
-bug_object.print_details()
-# turtle_object.print_details()
-# rover_object.print_details()
-
-bug_object.paint_vehicle("blue")
-
-bug_object.print_details()
